22dd03c4f6c9063a92142f040dd430e6eae5f8bd
angbbdalvz <angelito.abad.alvarez@gmail.com>
2025-10-24 17:40:35 -0500

    menu
    menu,volumen

--- Assets/scripts/Menu.cs
+++ Assets/scripts/Menu.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] private AudioMixer audioMixer;
     public void Jugar()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -17,4 +19,9 @@ public class Menu : MonoBehaviour
         Debug.Log("Saliendo del Juego...");
         Application.Quit();
     }
+    public void CambiarVolumen(float volumen)
+    {
+        audioMixer.SetFloat("Volumen", volumen);
+    }
+
 }
